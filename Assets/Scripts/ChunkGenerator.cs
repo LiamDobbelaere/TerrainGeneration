7d3e62f90ced639fd77797cb202e268c610fb84d
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-11-17 08:32:54 +0100

    Added chunk generator, better noise map

---
+++ Assets/Scripts/ChunkGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkGenerator : MonoBehaviour
+{
+    public GameObject proceduralTerrain;
+    public int seed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        for (int y = 0; y < 16; y++)
+        {
+            for (int x = 0; x < 16; x++)
+            {
+                GenerateChunk(x, y);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateChunk(int x, int y)
+    {
+        var terrainObject = Instantiate(proceduralTerrain, new Vector3(x * 64f, 0f, y * 64f), proceduralTerrain.transform.rotation);
+        var terrain= terrainObject.GetComponent<ProceduralTerrain>();
+
+        terrain.chunkX = x;
+        terrain.chunkY = y;
+        terrain.seed = seed;
+        terrain.xSize = 64;
+        terrain.ySize = 64;
+
+        terrain.Generate();
+    }
+}
--- Assets/Scripts/ProceduralTerrain.cs
+++ Assets/Scripts/ProceduralTerrain.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ProceduralTerrain : MonoBehaviour
@@ -8,9 +9,9 @@ public class ProceduralTerrain : MonoBehaviour
     public Texture2D heightmap;
     public bool useHeightmap = false;
     public int xSize, ySize;
-    private Vector3[] vertices;
-    private Color[] colors;
-    private int seed = 0;
+    public float chunkX, chunkY;
+    public float maxHeight = 50f;
+    public int seed = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -20,58 +21,57 @@ public class ProceduralTerrain : MonoBehaviour
 
     void Awake()
     {
+        if (!useHeightmap)
+        {
+            this.heightmap = new Texture2D(xSize - 1, ySize - 1);
+        }
+
         GetComponent<MeshFilter>().sharedMesh = new Mesh();
         GetComponent<MeshFilter>().mesh.name = "Procedural Terrain";
 
-        seed = Random.Range(0, 1000000);
-        Generate();
+        if (seed == -1)
+        {
+            seed = Random.Range(0, 100000);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
-        {
-            seed = Random.Range(0, 1000000);
-            Generate();
-        }
+
     }
 
-    void Generate()
+    public void Generate()
     {
-        MeshData terrain = new MeshData();
+        int increment = 2;
 
-        for (int y = 0; y < ySize - 1; y++)
+        MeshData terrain = new MeshData();
+        float[] heights = new float[xSize * ySize];
+        float[,] noiseMap = this.GenerateNoiseMap(seed, chunkX, chunkY, xSize + 1, ySize + 1, 50f, 4, 0.5f, 2f);
+
+        for (int y = 0; y < ySize; y += increment)
         {
-            for (int x = 0; x < xSize - 1; x++)
+            for (int x = 0; x < xSize; x += increment)
             {
-                float amplitude = 20f;
-                float noiseScale = 3f;
                 bool flip = x % 2 == 1;
                 if (y % 2 == 1)
                 {
                     flip = !flip;
                 }
 
-                float z = Mathf.PerlinNoise(
-                    seed + ((float)x / xSize * 2f) * noiseScale, 
-                    seed + ((float)y / ySize * 2f) * noiseScale);
-                float zNextX = Mathf.PerlinNoise(
-                    seed + ((float)(x + 1) / xSize * 2f) * noiseScale, 
-                    seed + ((float)y / ySize * 2f) * noiseScale);
-                float zNextY = Mathf.PerlinNoise(
-                    seed + ((float)x / xSize * 2f) * noiseScale, 
-                    seed + ((float)(y + 1) / ySize * 2f) * noiseScale);
-                float zNextXY = Mathf.PerlinNoise(
-                    seed + ((float)(x + 1) / xSize * 2f) * noiseScale, 
-                    seed + ((float)(y + 1) / ySize * 2f) * noiseScale);
+                float z = noiseMap[x, y];
+                float zNextX = noiseMap[x + increment, y];
+                float zNextY = noiseMap[x, y + increment];
+                float zNextXY = noiseMap[x + increment, y + increment];
+
+                heights[x + y * (ySize - 1)] = z;
 
                 if (useHeightmap)
                 {
                     z = heightmap.GetPixel(x, y).r;
-                    zNextX = heightmap.GetPixel(x + 1, y).r;
-                    zNextY = heightmap.GetPixel(x, y + 1).r;
-                    zNextXY = heightmap.GetPixel(x + 1, y + 1).r;
+                    zNextX = heightmap.GetPixel(x + increment, y).r;
+                    zNextY = heightmap.GetPixel(x, y + increment).r;
+                    zNextXY = heightmap.GetPixel(x + increment, y + increment).r;
                 }
 
                 Color c1 = Color.Lerp(GetColorForHeight(z), Color.white, Random.Range(0.0f, 0.12f));
@@ -80,42 +80,41 @@ public class ProceduralTerrain : MonoBehaviour
                 if (flip)
                 {
                     terrain.AddTriangle(
-                        new Vector3(x, z * amplitude, y),
-                        new Vector3(x, zNextY * amplitude, y + 1),
-                        new Vector3(x + 1, zNextXY * amplitude, y + 1),
+                        new Vector3(x, z * maxHeight, y),
+                        new Vector3(x, zNextY * maxHeight, y + increment),
+                        new Vector3(x + increment, zNextXY * maxHeight, y + increment),
                         c1);
                     terrain.AddTriangle(
-                        new Vector3(x, z * amplitude, y),
-                        new Vector3(x + 1, zNextXY * amplitude, y + 1),
-                        new Vector3(x + 1, zNextX * amplitude, y),
+                        new Vector3(x, z * maxHeight, y),
+                        new Vector3(x + increment, zNextXY * maxHeight, y + increment),
+                        new Vector3(x + increment, zNextX * maxHeight, y),
                         c2);
                 }
                 else
                 {
                     terrain.AddTriangle(
-                        new Vector3(x, z * amplitude, y),
-                        new Vector3(x, zNextY * amplitude, y + 1),
-                        new Vector3(x + 1, zNextX * amplitude, y),
+                        new Vector3(x, z * maxHeight, y),
+                        new Vector3(x, zNextY * maxHeight, y + increment),
+                        new Vector3(x + increment, zNextX * maxHeight, y),
                         c1);
                     terrain.AddTriangle(
-                        new Vector3(x + 1, zNextX * amplitude, y),
-                        new Vector3(x, zNextY * amplitude, y + 1),
-                        new Vector3(x + 1, zNextXY * amplitude, y + 1),
+                        new Vector3(x + increment, zNextX * maxHeight, y),
+                        new Vector3(x, zNextY * maxHeight, y + increment),
+                        new Vector3(x + increment, zNextXY * maxHeight, y + increment),
                         c2);
                 }
             }
         }
 
         GetComponent<MeshFilter>().mesh = terrain.ToMesh();
+
+        this.heightmap.SetPixels(heights.Select(height => new Color(height, height, height, 1f)).ToArray());
+        this.heightmap.Apply();
     }
 
     private Color GetColorForHeight(float height)
     {
-        if (height < 0.2f)
-        {
-            return new Color(0, 0.7f, 0.7f);
-        }
-        else if (height < 0.3f)
+        if (height < 0.3f)
         {
             return new Color(0.761f, 0.698f, 0.502f);
         }
@@ -131,5 +130,49 @@ public class ProceduralTerrain : MonoBehaviour
         {
             return Color.white;
         }
+    }
+
+    private float[,] GenerateNoiseMap(
+        int seed, float chunkX, float chunkY, int width, int height, 
+        float scale, int octaves, float persistence, float lacunarity)
+    {
+        float[,] noiseMap = new float[width, height];
+
+        float maxNoiseHeight = 1f;
+        float minNoiseHeight = -1f;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float amplitude = 1f;
+                float frequency = 1f;
+                float noiseHeight = 0f;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = (x + chunkX * xSize) / scale * frequency;
+                    float sampleY = (y + chunkY * ySize) / scale * frequency;
+
+                    float perlin = Mathf.PerlinNoise(seed + sampleX, seed + sampleY) * 2f - 1f;
+                    noiseHeight += perlin * amplitude;
+
+                    amplitude *= persistence;
+                    frequency *= lacunarity;
+                }
+
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+
+        return noiseMap;
     }
 }
